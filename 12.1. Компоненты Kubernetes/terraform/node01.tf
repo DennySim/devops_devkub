f1505137004e5c079cb0d2e0fcb8452dda256699
Denis Simankin <x-modem@yandex.ru>
2022-06-03 13:34:41 +0300

    дз 12.1

---
+++ 12.1. Компоненты Kubernetes/terraform/node01.tf
@@ -0,0 +1,28 @@
+resource "yandex_compute_instance" "node01" {
+  zone                      = "ru-central1-a"
+  hostname                  = "node01.netology.cloud"
+  allow_stopping_for_update = true
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id    = "fd89ovh4ticpo40dkbvd"
+      name        = "root-node01"
+      type        = "network-nvme"
+      size        = "30"
+    }
+  }
+
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.default.id}"
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
